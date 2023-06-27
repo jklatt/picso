3a548476e791dde040e05719afe7d2b432957742
JK <juliane.klatt@pm.me>
2023-06-27 11:48:22 +0200

    added skeleton functions

--- picso.py
+++ picso.py
@@ -1,6 +1,7 @@
 import numpy
+import json
 
-def load(file_directory,file_extension):
+def load(file_directory, file_extension):
     """Loads all file names within a given directory and with a given extension into a dictionary"""
     dictionary_of_objects = {}
     return dictionary_of_objects
@@ -9,10 +10,15 @@ def initialize(dictionary_of_objects):
     """Randomly assigns a unique integer score to each of the objects in a dictionary"""
     return dictionary_of_objects
 
-def switch(object1,object2):
-    """Switches scores of two objects"""
-    return object1,object2
+def select(dictionary_of_objects):
+    """Randomly selects two of the objects in a dictionary"""
+    (object1, object2) = ()
+    return (object1, object2)
 
-def compare(object1,object2):
+def compare(object1, object2):
     """Facilitates user-based comparison of two objects"""
-    return object1,object2
+    return (object1, object2)
+
+def switch(object1, object2):
+    """Switches scores of two objects"""
+    return (object1, object2)

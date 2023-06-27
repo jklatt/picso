31744b266b023f109af36784a92051003d3019d0
JK <juliane.klatt@pm.me>
2023-06-27 14:31:33 +0200

    added data for test fixture and test_load()

--- tests/test_picso.py
+++ tests/test_picso.py
@@ -2,6 +2,17 @@ from unittest import TestCase
 from picso import Album
 
 class PicsoTestCase(TestCase):
+    def setUp(self):
+        self.album = Album()
 
+    def tearDown(self):
+        self.album = None
+
+    def test_init(self):
+        self.assertEqual(self.album.title,'New Album')
+        self.assertEqual(self.album.size,0)
     def test_load(self):
-        return None
+        self.album.load('~/home/github/picso/tests/fixtures/data','.png')
+        self.assertIn('file1.png', self.album.content)
+        self.assertNotIn('file1.txt', self.album.content)
+        self.assertEqual(self.album.size, 5)
